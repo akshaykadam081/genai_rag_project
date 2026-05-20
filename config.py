fd98ee04a993d8eb65bcbbe4c258facfe688c894
akshaykadam081 <akshay.kadam081@gmail.com>
2026-05-20 21:43:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
 GROQ_API_KEY = "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"
-
-MODEL_NAME = "llama3-8b-8192"
-
+MODEL_NAME ="llama-3.1-8b-instant"
 DATA_PATH = "data/new_articles.zip"
-
 CHROMA_PATH = "chroma_db"
