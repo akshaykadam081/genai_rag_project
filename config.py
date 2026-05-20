448676be2b20cf75894bb8787782c60b96e84c7c
akshaykadam081 <akshay.kadam081@gmail.com>
2026-05-20 22:28:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-GROQ_API_KEY = "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"
+GROQ_API_KEY = "your_api_key_here"
 MODEL_NAME ="llama-3.1-8b-instant"
 DATA_PATH = "data/new_articles.zip"
 CHROMA_PATH = "chroma_db"
